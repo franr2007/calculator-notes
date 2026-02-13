90a99bbf4dd4a382d8b24deedcb6adebb8ad13cd
fran <frodriguez4@nigul.cide.es>
2026-02-13 16:34:35 +0100

    funcionalidad hecha

--- Actividad-Ramas-y-PR/src/App.java
+++ Actividad-Ramas-y-PR/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Pon la nota del examen 1: ");
+        double examen1=sc.nextInt();
+        System.out.println("Pon la nota del examen 2: ");
+        double examen2=sc.nextInt();
+        System.out.println("Pon la nota del examen 3: ");
+        double examen3=sc.nextInt();
+        System.out.println("Pon la nota del examen 4: ");
+        double examen4=sc.nextInt();
+
+        
     }
 }
